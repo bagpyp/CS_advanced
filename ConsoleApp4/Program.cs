3f7f9b8a8298fee095e4a8286e04eabfe1ad8f3d
Robert Cunningham <robert@bagpyp.net>
2019-12-09 11:31:20 -0800

    commit

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Add Five Or...!");
+            Console.WriteLine("Enter a number to have Five added to.");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("If you'd like me to add a different number to " + input1
+                + ", enter it now. Otherwise, press Enter and I'll show you what you've got!");
+            string input2 = Console.ReadLine();
+
+            Test test = new Test();
+            if (input2 == "")
+            {
+                Console.WriteLine("5 plus " + input1 + " equals " + test.AddFiveOr(input1));
+            }
+            else
+            {
+                Console.WriteLine(input1 + " plus " + input2 + " equals " + test.AddFiveOr(input1, Convert.ToInt32(input2)));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp4/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Test
+    {
+        public int AddFiveOr(int one, int two = 5)
+        {
+            int _ = one + two;
+            return _;
+        }
+            
+    }
+}
