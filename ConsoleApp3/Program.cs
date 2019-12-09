7509a276aef649723262d7782a7f063bc9b97e53
Robert Cunningham <robert@bagpyp.net>
2019-12-09 11:15:38 -0800

    add another weird one

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main()
+        {
+            RootFinder rootFinder = new RootFinder();
+
+            int result = rootFinder.Find(12);
+            Console.WriteLine(result);
+
+            result = rootFinder.Find(1212.12m);
+            Console.WriteLine(result);
+
+            result = rootFinder.Find("12121212");
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp3/RootFinder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class RootFinder
+    {
+        public int Find(int input)
+        {
+            int result = Convert.ToInt32(Math.Sqrt(input));
+            return result;
+        }
+
+        public int Find(decimal input)
+        {
+            int result = Convert.ToInt32(Math.Sqrt(decimal.ToDouble(input)));
+            return result;
+        }
+
+        public int Find(string input)
+        {
+            int result = Convert.ToInt32(Math.Sqrt(Convert.ToDouble(input)));
+            return result;
+        }
+
+
+
+    }
+}
